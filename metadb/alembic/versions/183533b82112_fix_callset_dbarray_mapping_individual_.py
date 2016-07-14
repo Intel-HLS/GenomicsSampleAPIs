e8962a8667ba44e766bc9a6b05640f9240c8deb7
jacmarjorie <jacmarjorie@gmail.com>
2016-07-13 18:18:38 -0700

    cleaning up alembic files

--- metadb/alembic/versions/183533b82112_fix_callset_dbarray_mapping_individual_.py
+++ metadb/alembic/versions/183533b82112_fix_callset_dbarray_mapping_individual_.py
@@ -22,16 +22,14 @@ from metadb.models import autoinc_handler
 def upgrade():
     # Had to add this explicitly, because of the use of Sequence() in the SQLAlchemy model without
     # setting server default
-    op.alter_column('callset', 'id', server_default=sa.Sequence(
-        'callset_id_seq').next_value())
+    op.alter_column('callset', 'id', server_default=sa.Sequence('callset_id_seq').next_value())
     op.create_table('sample',
                     sa.Column('id', sa.BigInteger, nullable=False),
                     sa.Column('guid', sa.String(length=36), nullable=False),
                     sa.Column('individual_id', sa.BigInteger, nullable=False),
                     sa.Column('name', sa.Text(), nullable=False),
                     sa.Column('info', TextPickleType, nullable=True),
-                    sa.ForeignKeyConstraint(
-                        ['individual_id'], ['individual.id'], ),
+                    sa.ForeignKeyConstraint(['individual_id'], ['individual.id'],),
                     sa.PrimaryKeyConstraint('id'),
                     sa.UniqueConstraint('guid')
                     )
@@ -39,15 +37,11 @@ def upgrade():
                     sa.Column('callset_id', sa.BigInteger, nullable=False),
                     sa.Column('db_array_id', sa.BigInteger, nullable=False),
                     sa.Column('tile_row_id', sa.BigInteger, nullable=True),
-                    sa.ForeignKeyConstraint(['callset_id'], ['callset.id'], ),
-                    sa.ForeignKeyConstraint(
-                        ['db_array_id'], ['db_array.id'], ),
-                    sa.PrimaryKeyConstraint(
-                        'callset_id', 'db_array_id', name='primary_key')
-                    )
+                    sa.ForeignKeyConstraint(['callset_id'], ['callset.id'],),
+                    sa.ForeignKeyConstraint(['db_array_id'], ['db_array.id'],),
+                    sa.PrimaryKeyConstraint('callset_id', 'db_array_id', name='primary_key'))
     op.create_index(
-        'db_array_id_tile_row_id_idx', 'callset_to_db_array_association', [
-            'db_array_id', 'tile_row_id'], unique=True)
+        'db_array_id_tile_row_id_idx', 'callset_to_db_array_association',['db_array_id', 'tile_row_id'], unique=True)
     # Trigger for auto-incrementing tile_row_idx and num_rows
     op.execute('''\
     CREATE OR REPLACE FUNCTION increment_num_rows_in_db_array_pgsql()
@@ -61,21 +55,18 @@ def upgrade():
     CREATE TRIGGER increment_num_rows_in_db_array AFTER INSERT ON callset_to_db_array_association
     FOR EACH ROW EXECUTE PROCEDURE increment_num_rows_in_db_array_pgsql();
     ''')
-    op.add_column(u'callset', sa.Column(
-        'source_sample_id', sa.BigInteger, nullable=False))
-    op.add_column(u'callset', sa.Column(
-        'target_sample_id', sa.BigInteger, nullable=False))
+    op.add_column(u'callset', 
+        sa.Column('source_sample_id', sa.BigInteger, nullable=False))
+    op.add_column(u'callset', 
+        sa.Column('target_sample_id', sa.BigInteger, nullable=False))
     op.drop_constraint(u'callset_dbrow_id_fkey', 'callset', type_='foreignkey')
-    op.drop_constraint(u'callset_individual_id_fkey',
-                       'callset', type_='foreignkey')
-    op.create_foreign_key('callset_source_sample_id_fkey',
-                          'callset', 'sample', ['source_sample_id'], ['id'])
-    op.create_foreign_key('callset_target_sample_id_fkey',
-                          'callset', 'sample', ['target_sample_id'], ['id'])
+    op.drop_constraint(u'callset_individual_id_fkey','callset', type_='foreignkey')
+    op.create_foreign_key('callset_source_sample_id_fkey','callset', 'sample', ['source_sample_id'], ['id'])
+    op.create_foreign_key('callset_target_sample_id_fkey','callset', 'sample', ['target_sample_id'], ['id'])
     op.drop_column(u'callset', 'dbrow_id')
     op.drop_column(u'callset', 'individual_id')
-    op.add_column(u'db_array', sa.Column(
-        'num_rows', sa.BigInteger, nullable=False))
+    op.add_column(u'db_array', 
+        sa.Column('num_rows', sa.BigInteger, nullable=False))
     op.drop_table('db_row')
 
 
@@ -86,47 +77,21 @@ def downgrade():
     op.drop_column(u'db_array', 'num_rows')
     op.create_table(
         'db_row',
-        sa.Column(
-            'id',
-            sa.BIGINT(),
-            nullable=False),
-        sa.Column(
-            'db_array_id',
-            sa.BIGINT(),
-            autoincrement=False,
-            nullable=False),
-        sa.Column(
-            'tile_row_id',
-            sa.BIGINT(),
-            autoincrement=False,
-            nullable=False),
-        sa.ForeignKeyConstraint(
-            ['db_array_id'],
-            [u'db_array.id'],
-            name=u'db_row_db_array_id_fkey'),
-        sa.PrimaryKeyConstraint(
-            'id',
-            name=u'db_row_pkey'))
+        sa.Column('id', sa.BIGINT(), nullable=False),
+        sa.Column('db_array_id', sa.BIGINT(), autoincrement=False, nullable=False),
+        sa.Column('tile_row_id', sa.BIGINT(), autoincrement=False, nullable=False),
+        sa.ForeignKeyConstraint(['db_array_id'],[u'db_array.id'], name=u'db_row_db_array_id_fkey'),
+        sa.PrimaryKeyConstraint('id', name=u'db_row_pkey'))
     op.add_column(
         u'callset',
-        sa.Column(
-            'individual_id',
-            sa.BIGINT(),
-            autoincrement=False,
-            nullable=False))
-    op.add_column(u'callset', sa.Column('dbrow_id', sa.BIGINT(),
-                                        autoincrement=False, nullable=False))
-    op.drop_constraint('callset_source_sample_id_fkey',
-                       'callset', type_='foreignkey')
-    op.drop_constraint('callset_target_sample_id_fkey',
-                       'callset', type_='foreignkey')
-    op.create_foreign_key(u'callset_individual_id_fkey',
-                          'callset', 'individual', ['individual_id'], ['id'])
-    op.create_foreign_key(u'callset_dbrow_id_fkey',
-                          'callset', 'db_row', ['dbrow_id'], ['id'])
+        sa.Column('individual_id', sa.BIGINT(), autoincrement=False, nullable=False))
+    op.add_column(u'callset', sa.Column('dbrow_id', sa.BIGINT(), autoincrement=False, nullable=False))
+    op.drop_constraint('callset_source_sample_id_fkey','callset', type_='foreignkey')
+    op.drop_constraint('callset_target_sample_id_fkey','callset', type_='foreignkey')
+    op.create_foreign_key(u'callset_individual_id_fkey','callset', 'individual', ['individual_id'], ['id'])
+    op.create_foreign_key(u'callset_dbrow_id_fkey','callset', 'db_row', ['dbrow_id'], ['id'])
     op.drop_column(u'callset', 'target_sample_id')
     op.drop_column(u'callset', 'source_sample_id')
-    op.drop_index('db_array_id_tile_row_id_idx',
-                  table_name='callset_to_db_array_association')
+    op.drop_index('db_array_id_tile_row_id_idx',table_name='callset_to_db_array_association')
     op.drop_table('callset_to_db_array_association')
     op.drop_table('sample')
