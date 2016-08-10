fb2419f2411a5c1a49d004051ca25757a60b6caa
Brian L. Cremeans <gitmach@gmail.com>
2016-08-10 10:33:45 -0700

    some path fixes

---
+++ CONTRIBUTING.md
@@ -0,0 +1,26 @@
+#How to contribute
+
+## Thank you!
+
+We are excited you are interested in improving the sample APIs  Thanks for taking the interest and time!
+
+## Code Layout
+
+## Code Style
+
+## Pull Request Workflow
+
+## Testing
+
+## Questions
+
+### FAQ
+To be added as questions are frequently asked
+
+### Answers
+
+If you have any questsions, checkout out the [Questions](#Questions) section.  Most of our documentation is in the project [wiki](https://github.com/Intel-HLS/GenomicsSampleAPIs/wiki)
+
+If you have any further questions, contact the project [maintainers or authors](https://github.com/Intel-HLS/GenomicsSampleAPIs/AUTHORS).
+
+
--- infrastructure/ansible/roles/genomicsdb/spec/genomicsdb_spec.rb
+++ infrastructure/ansible/roles/genomicsdb/spec/genomicsdb_spec.rb
@@ -12,32 +12,20 @@ describe file('/home/genomicsdb/repos/GenomicsSampleAPIs/web/templates') do
   it { should be_directory }
 end
 
-describe file('/home/genomicsdb/repos/TileDB') do
+describe file('/home/genomicsdb/repos/GenomicsSampleAPIs/dependencies/GenomicsDB') do
   it { should be_directory }
 end
 
-describe file('/home/genomicsdb/repos/TileDB/tiledb_cmd/bin/release/tiledb_load_csv') do
-  it { should be_file }
-end
-
-describe file('/home/genomicsdb/repos/htslib') do
+describe file('/home/genomicsdb/repos/search_library/dependencies/GenomicsDB/dependencies/htslib') do
   it { should be_directory }
 end
 
-describe file('/home/genomicsdb/repos/rapidjson') do
+describe file('/home/genomicsdb/repos/search_library/dependencies/GenomicsDB/dependencies/rapidjson') do
   it { should be_directory }
 end
 
 describe file('/home/genomicsdb/DB') do
   it { should be_directory }
-end
-
-describe command('sudo -u ga4gh_writer -i psql -l | grep metadb | wc -l') do
-  its(:stdout) { should match /1/}
-end
-
-describe command('sudo -u ga4gh_writer -i psql -c "\dt+" -d metadb | grep table | wc -l') do
-  its(:stdout) { should match /15/}
 end
 
 describe user('ga4gh_writer') do
@@ -50,3 +38,10 @@ describe user('ga4gh_client') do
   it { should belong_to_group 'ga4gh' }
 end
 
+describe command('sudo -u ga4gh_writer -i psql -l | grep metadb | wc -l') do
+  its(:stdout) { should match /1/}
+end
+
+describe command('sudo -u ga4gh_writer -i psql -c "\dt+" -d metadb | grep table | wc -l') do
+  its(:stdout) { should match /15/}
+end
