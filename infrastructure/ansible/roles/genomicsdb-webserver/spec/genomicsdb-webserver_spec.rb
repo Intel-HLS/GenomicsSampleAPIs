314d47703f3413d245ff966c10a9722fe4e1351b
Brian L. Cremeans <gitmach@gmail.com>
2016-08-11 22:50:53 -0700

    restore test count

--- infrastructure/ansible/roles/genomicsdb-webserver/spec/genomicsdb-webserver_spec.rb
+++ infrastructure/ansible/roles/genomicsdb-webserver/spec/genomicsdb-webserver_spec.rb
@@ -34,7 +34,6 @@ describe port(8990) do
   it { should be_listening }
 end
 
-describe command('curl -H "Content-Type: application/json" -X POST -d \'{"end": 27507000, "pageSize": 31, "start": 27506000, "pageToken": null, "variantSetIds": [], "variantName": null, "referenceName": "13"}\'  localhost:8990/variants/search') do
-#describe command('curl -H "Content-Type: application/json" -X POST -d \'{"end": 27507000, "pageSize": 31, "start": 27506000, "pageToken": null, "variantSetIds": [], "variantName": null, "referenceName": "13"}\'  localhost:8990/variants/search | grep alternateBases | wc -l') do
-  its(:stdout) { should match /somethingishouldneversee/}
+describe command('curl -H "Content-Type: application/json" -X POST -d \'{"end": 27507000, "pageSize": 31, "start": 27506000, "pageToken": null, "variantSetIds": [], "variantName": null, "referenceName": "13"}\'  localhost:8990/variants/search | grep alternateBases | wc -l') do
+  its(:stdout) { should match /2/}
 end
